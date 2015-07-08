cfb65ffc63525edeeb4d91750e9e02809bd800e7
Yvan BARTHÉLEMY <yvan@agorize.com>
2015-07-08 14:18:42 +0200

    Custom functions

--- custom/example.zsh
+++ custom/example.zsh
@@ -3,3 +3,16 @@
 #
 # brainstormr=/Users/robbyrussell/Projects/development/planetargon/brainstormr
 #
+
+reset_db() {
+	powder stop
+	spring stop
+	bundle exec rake db:drop:all db:create:all db:migrate
+	bundle exec rake db:migrate RAILS_ENV=test
+	powder start
+}
+
+run_test() {
+  COVERAGE=true rspec &&
+  rubocop -f fu --auto-correct
+}
